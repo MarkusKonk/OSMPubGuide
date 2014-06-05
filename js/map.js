71fe2c1309000d8eacdd7ba4d3690e0759ee94cb
Jana <j_klem04@uni-muenster.de>
2014-06-05 11:38:47 +0200

    enhance popup

--- js/map.js
+++ js/map.js
@@ -28,9 +28,19 @@
 					attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 				}).addTo(map);
 				
-				L.marker([51.9629, 7.6286]).addTo(map)
-				.bindPopup("Name</b><br />Opening hours</b><br />Adress</b><br />Website</b><br /><a href='#'>Link for more information</></b><br /><img src='gorilla.jpg' style = 'height:50px;'/>");
-
+			  marker=L.marker();
+				 
+				 marker.setLatLng( [51.9629, 7.6286] );
+				$('#mapElements').append("<div data-role='popup' href='popupBasic' id='popupArrow' class='ui-content' data-arrow='true'><p align='center'><a>Gorilla Bar</a></p><table style='border-spacing: 15px 0px'><tr><td valign='top'><b>Opening hours </b> </td><td><table><tr><td>Mon - Thu:</td><td> 20.00 - 02.00</td><tr><td>Fri - Sat:</td><td> 20.00 - 03.00 </td></tr> </table></td></tr><tr><td valign='top'><b>Adress</b></td><td>Juedefelderstr. 54<br>48143 MUENSTER  </td></tr><tr><td valign='top'><b>Phone number</b></td><td>0251-4882188</td></tr><tr><td valign='top'><b>Mail adress</b></td><td>info@gorilla-bar.de</td></tr><tr valign='top'><td><b>Website</b></td><td><a href='http://www.gorilla-bar.de/' style='font-weight:normal'>http://www.gorilla-bar.de/</a>  </td></tr><tr><th  colspan='2' align='left'><a href='http://www.gorilla-bar.de/'>More information</a></th></tr></table><p align='center'><img src='gorilla1.jpg' style = 'height:80px;'/><img src='gorilla2.jpg' style = 'height:80px;'/></p></div>");
+               
+			   marker.on('click', function(e) {
+			   var m=e.target;
+			   var x=map.latLngToContainerPoint(m.getLatLng(),zoom).x;
+			   var y =map.latLngToContainerPoint(m.getLatLng(),zoom).y;
+				var offsetLeft=$('#leftpanel2').width();
+				$('#popupArrow').popup( 'open',{x:offsetLeft+x,y:y});
+                 });
+				marker.addTo(map);
 				var popup = L.popup();
 				
 				function onMapClick(e) {
