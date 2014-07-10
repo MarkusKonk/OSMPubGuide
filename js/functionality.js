64c72a6e3fb3d45241dd32a5e03d95b0f921a726
Markus <mkonkol@arcor.de>
2014-07-10 19:10:06 +0200

    picture bug

--- js/functionality.js
+++ js/functionality.js
@@ -186,10 +186,10 @@
 			$("#query").collapsible("option", "collapsed", true);
 			document.getElementById("result_text").style.display = "block";
 			deleteResults();
-
+			
 			for (var i = 0; i < pubArray.length; i++){
 				var pictureID;
-				if ( !doesFileExist("http://localhost/OSMPubGuide/pubs/"+pubArray[i].id + ".png")){
+				if ( !doesFileExist("http://giv-openpubguide.uni-muenster.de/pubs/"+pubArray[i].id + ".png")){
 					pictureID = 0;
 				}
 				else{
