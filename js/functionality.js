e304d5ba5fbc2ce2e0ec069e452d8003dafec2c2
Jana <j_klem04@uni-muenster.de>
2014-07-14 18:29:11 +0200

    add "http://" to website link

--- js/functionality.js
+++ js/functionality.js
@@ -98,7 +98,9 @@
 		        openingHours = openingHours + " </table>";
 		    }
 			var picture="";
-	        
+	        if(website.search("http")==-1){
+			website="http://"+website;
+			}
 			//create popup element
 		    var popup = "<div data-role='popup' id='popup_" + id + "' class='ui-content ' data-arrow='true'><a data-rel='back' data-role='button' data-theme='a' data-icon='delete' data-iconpos='notext' class='ui-btn-right'/><p align='center'><a>" + pubName + "</a></p><table style='border-spacing: 15px 0px'><tr><td valign='top'><b>Time until close</b> </td><td>" + tuc + "</td></tr><tr><td valign='top'><b>Opening hours </b> </td><td>" + openingHours + "</td></tr><tr><td valign='top'><b>Adress</b></td><td>" + adress + " </td></tr><tr><td valign='top'><b>Phone number</b></td><td>" + phone + "</td></tr><tr><td valign='top'><b>Mail adress</b></td><td>" + e_mail + "</td></tr><tr valign='top'><td><b>Website</b></td><td><a href='" + website + "' style='font-weight:normal'>" + website + "</a>  </td></tr><tr><th  colspan='2' align='left'><a id='popupResultLink_" + id + "'>More information</a></th></tr></table><p id='picture_"+id+"' align='center'>" + picture+ "</p></div>";
 		    popups.push(popup);
